03e222fe2752bfffaa00ff05f0f8356f2fd43bc5
Victor Powilleit <victor.powilleit@gmail.com>
2021-05-18 19:34:21 -0300

    finalizado (versão 1)

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import React, {createContext, ReactNode, useEffect, useState} from 'react'
-import { api } from './services/api'
+import {createContext, ReactNode, useContext, useEffect, useState} from 'react'
+import { api } from '../services/api'
 
 interface Transaction {
   id: number
@@ -21,7 +21,7 @@ interface TransactionsContextData {
   createTransaction: (transaction:TransactionInput)=>Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
 
 export function TransactionsProvider({children}: TransactionProviderProps){
   const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -42,4 +42,9 @@ export function TransactionsProvider({children}: TransactionProviderProps){
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions(){
+  const context = useContext(TransactionsContext)
+  return context
 }
